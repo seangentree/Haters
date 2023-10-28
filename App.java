53ad58aedc1b67f77cf1a8898102bf87bee00f57
sociosloth <134299181+sociosloth@users.noreply.github.com>
2023-10-28 16:45:00 -0500

    Add files via upload
    updated version of app.data for testing

--- App.java
+++ App.java
@@ -1,35 +1,67 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+
+        
         System.out.println("Labeling Program");
-        ProcessInput();
+        System.out.print("Enter the text file path of data: ");
+        File inputFile = new File(input.next());
+        input.nextLine();
+
+        while (!inputFile.exists()) {
+            System.out.print("File cannot be found, please try again");
+            inputFile = new File(input.next());
+            input.nextLine();
+
+        }
+
+        System.out.println("Found file \"" + inputFile + "\"");
+        System.out.println("Enter end file name: ");
+        File outputFile = new File(input.next());
+        input.nextLine();
+
+        boolean override = false;
+        while (outputFile.exists() && !override) {
+            System.out.println("This file exists, would you like to override it? (Y/N): ");
+            char response = input.next().trim().toUpperCase().charAt(0);
+
+            override = response == 'Y';
+
+            if (!override) {
+                System.out.println("Enter new end file name: ");
+                outputFile = new File(input.next().trim());
+            }
+        }
+
+        ProcessInput(inputFile, outputFile);
+
     }
 
-    public static void ProcessInput () throws FileNotFoundException {
+    /** For Peter
+     * 
+     * @throws FileNotFoundException
+     */
+    public static void ProcessInput (File inputFile, File outputFile) throws FileNotFoundException {
 
-        String vandyText = "C:\\Users\\peter\\Downloads\\VandyHacks\\src\\DATA for VANDYHACKS_ActualFile.txt";
-        File inputFile = new File(vandyText);
-        Scanner scnr = new Scanner(inputFile);
-
-        String fileOutput = "C:\\Users\\peter\\Downloads\\VandyHacks\\src\\Empty Sheet.txt";
-        File outputFile = new File(fileOutput);
+        Scanner fileScnr = new Scanner(inputFile);
         PrintStream excelOutput = new PrintStream(outputFile);
 
         int indivNum = 0;
 
-        while (scnr.hasNext()) {
+        while (fileScnr.hasNext()) {
             String input1 = scnr.nextLine();
             excelOutput.println(indivNum + " - " + input1);
             indivNum +=1;
         }
 
-        scnr.close();
+        fileScnr.close();
         excelOutput.close();
 
     }
+
+    
 
 }
